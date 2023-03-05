1fc600df89687113af35c94624e3e144e0019957
CatasArt <119759210+CatasArt@users.noreply.github.com>
2023-03-05 19:48:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="revChatGPT",
+<<<<<<< HEAD
     version="2.3.5",
     license="GNU General Public License v2.0",
+=======
+    version="3.1.1",
+    description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/acheong08/ChatGPT",
+>>>>>>> 925bef9abdce6232a3ec028c128c81018aad2a10
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
-    description="ChatGPT is a reverse engineering of OpenAI's ChatGPT API",
+    license="GNU General Public License v2.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=["Unofficial", "V2", "V1"],
-    url="https://github.com/acheong08/ChatGPT",
+    py_modules=["Unofficial", "V2", "V1", "V0", "V3"],
     install_requires=[
         "OpenAIAuth==0.3.2",
+<<<<<<< HEAD
         "requests",
+=======
+        "requests[socks]",
+        "httpx[socks]",
+        "prompt-toolkit",
+        "tiktoken",
+>>>>>>> 925bef9abdce6232a3ec028c128c81018aad2a10
     ],
     extras_require={
         "unofficial": [
@@ -22,9 +35,10 @@ setup(
             "undetected_chromedriver",
             "selenium",
             "tls_client",
-            "requests",
+        ],
+        "official": [
+            "openai",
+            "tiktoken",
         ],
     },
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
 )
